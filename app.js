c91795ed0c6ee0ffcffecd2cba4b673c9fc4e929
Aniket Kadam <aniketkadam47590@gmail.com>
2023-05-03 00:40:02 +0530

    Level 1 - Username and Password Only

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
